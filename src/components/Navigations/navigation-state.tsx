d83121e28728fa4ee72e7e3d52a6d2060f25b5c5
rijalBinHusen <jeryzal333@gmail.com>
2023-10-05 07:51:00 +0700

    styling container class

--- src/components/Navigations/navigation-state.tsx
+++ src/components/Navigations/navigation-state.tsx
@@ -2,6 +2,6 @@ import { createSignal } from "solid-js";
 
 type page = "home"|"item"|"stock";
   
-const currentPage = <page|"home">;
+const currentPage:page = "home";
 
 export const [page, setPage] = createSignal(currentPage);
