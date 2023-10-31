49a3b1212b6ed5c6367823dfe470f610d467d585
rijalBinHusen <jeryzal333@gmail.com>
2023-10-31 16:17:15 +0700

    stockopname form in progress

--- src/pages/stock-opname-lists/StockOpnameForm.tsx
+++ src/pages/stock-opname-lists/StockOpnameForm.tsx
@@ -1,35 +1,54 @@
-import { Accessor,  JSX, Setter, Show } from 'solid-js';
+import { Accessor,  JSX, Setter, Show, createSignal } from 'solid-js';
+import { Stock } from "./function";
 
-export interface handleFormFolderProps {
-  handleFolder: Function
-  folderName: Accessor<string>
-  setFolder: Setter<string>
-}
+// export interface handleFormFolderProps {
+//   handleFolder: Function
+//   folderName: Accessor<string>
+//   setFolder: Setter<string>
+// }
 
-function FormFolder (props: handleFormFolderProps) {
+// function FormFolder (props: handleFormFolderProps) {
+function StockForm () {
 
-  const handleFormFolder: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
+  const [stock, setStock] = createSignal<Stock>({
+    addition_stock: 0,
+    date_stock: '',
+    folder_id: '',
+    height_stock: 0,
+    hole_stock: 0,
+    itemId: '',
+    length_stock: 0,
+    stockId: '',
+    width_stock: 0
+  });
+
+  // const handleFormFolder: JSX.EventHandler<HTMLButtonElement, MouseEvent> = (event) => {
     
-    event.preventDefault();
-    props.handleFolder();
-  }
+  //   event.preventDefault();
+  //   props.handleFolder();
+  // }
 
   return (
-    
-    <div class="form-input">
 
-      <input 
-          type="text" 
-          class="folder-name" 
-          name="folder-name" 
-          id="folder-name" 
-          placeholder="Nama folder baru" 
-          value={props.folderName()} 
-          onInput={(e) => props.setFolder(e.currentTarget.value)}
-        />
-      <button onClick={handleFormFolder} class="button">Cari</button>
+    <div class="modal-stock-opname">
+      <h2>Tambahkan stock baru</h2>
+      <div class="form-stock-opname">
+
+        <label for="product-name">Nama produk</label>
+        <input id="product-name" type="text" placeholder="Nama item"/>
+        <label for="width-stack">Lebar penataan</label>
+        <input id="width-stack" type="number" placeholder="Lebar (Kesamping)"/>
+        <label for="height-stack">Tinggi penataan</label>
+        <input id="height-stack" type="number" placeholder="Tinggi (Keatas)"/>
+        <label for="long-stack">Panjang penataan</label>
+        <input id="long-stack" type="number" placeholder="Panjang (Kebelakang)"/>
+        <label for="hole-stack">Penataan Lubang (Kosong)</label>
+        <input id="hole-stack" type="number" placeholder="Lubang"/>
+        
+        <input type="button" class="secondary-color" value="Tambahkan"/>
+      </div>
     </div>
   );
 };
 
-export default FormFolder;
+export default StockForm;
